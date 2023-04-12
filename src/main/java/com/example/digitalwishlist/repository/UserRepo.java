a55c7ca6f7308cfc88a0c1eb97826bf4fbbc035b
NoahWederking <noah376b@stud.kea.dk>
2023-04-12 11:26:15 +0200

    creating new user

--- src/main/java/com/example/digitalwishlist/repository/UserRepo.java
+++ src/main/java/com/example/digitalwishlist/repository/UserRepo.java
@@ -1,2 +1,21 @@
-package com.example.digitalwishlist.repository;public class UserRepo {
+package com.example.digitalwishlist.repository;
+
+import com.example.digitalwishlist.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepo {
+
+    @Autowired
+    JdbcTemplate template;
+
+
+    public void addUser(User user){
+        String sql = "INSERT INTO user (user_id, user_name, user_password, user_email) VALUES(?,?,?,?)";
+        template.update(sql,user.getUser_id(),user.getUser_name(), user.getUser_password(), user.getUser_email());
+    }
+
+
 }
