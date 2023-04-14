71e88366781a9fc92988babbf0685043a06b1146
NoahWederking <noah376b@stud.kea.dk>
2023-04-14 11:41:39 +0200

    making item, wish and wishItem repos and service

--- src/main/java/com/example/digitalwishlist/service/ItemService.java
+++ src/main/java/com/example/digitalwishlist/service/ItemService.java
@@ -1,2 +1,28 @@
-package com.example.digitalwishlist.service;public class ItemService {
+package com.example.digitalwishlist.service;
+
+import com.example.digitalwishlist.model.Item;
+import com.example.digitalwishlist.repository.ItemRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ItemService {
+
+    @Autowired
+    ItemRepo itemRepo;
+
+    public void addItem(int item_name, int item_price){
+        itemRepo.addItem(item_name,item_price);
+    }
+
+    public List<Item> getItems(){
+         return itemRepo.getItems();
+    }
+
+    public int getNewestItemId(){
+        return itemRepo.getNewestItemId();
+    }
+
 }
--- src/main/java/com/example/digitalwishlist/service/WishService.java
+++ src/main/java/com/example/digitalwishlist/service/WishService.java
@@ -1,2 +1,19 @@
-package com.example.digitalwishlist.service;public class WishService {
+package com.example.digitalwishlist.service;
+
+import com.example.digitalwishlist.repository.WishRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WishService {
+
+    @Autowired
+    WishRepo wishRepo;
+
+
+    public void addWish(int wishlist_id, int item_id){
+        wishRepo.addWish(wishlist_id,item_id);
+    }
+
+
 }
